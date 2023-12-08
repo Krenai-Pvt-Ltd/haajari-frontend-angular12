1e29aa7b154b8f0e4cb9527b78da79e977aec73f
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2023-12-08 18:56:58 +0530

    bug fixed

---
+++ src/app/modules/common/notifaction-tost/notifaction-tost.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotifactionTostComponent } from './notifaction-tost.component';
+
+describe('NotifactionTostComponent', () => {
+  let component: NotifactionTostComponent;
+  let fixture: ComponentFixture<NotifactionTostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotifactionTostComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotifactionTostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
