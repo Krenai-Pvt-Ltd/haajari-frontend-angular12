c6b7442f54befe995987e4d3ca7c7d7d98e1fb54
Shivendra Shrivastwa <shivendra@krenai.com>
2023-12-19 17:44:20 +0530

    bug fixed

---
+++ src/app/models/logged-in-user.ts
@@ -0,0 +1,9 @@
+export class LoggedInUser {
+    uuid : string = '';
+    name : string = '';
+    email : string = '';
+    role : string = '';
+    orgRefId : string = '';
+    statusResponse : string = '';
+    httpCustomStatus : string = '';
+}
