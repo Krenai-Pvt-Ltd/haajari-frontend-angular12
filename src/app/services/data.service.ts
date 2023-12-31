c76af225daf61e02e1f9a923f508474ad666e66e
Richa <richa@krenai.com>
2023-12-31 23:47:09 +0530

    commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -162,6 +162,18 @@ export class DataService {
     .set('search_by', searchBy);
     return this.httpClient.get<any>(`${this.baseUrl}/users/get/by-filters`, {params});
   }
+
+  getUsersByFilterForLeaveSetting(itemPerPage: number, pageNumber: number, sort: string, sortBy: string, search: string, searchBy: string) : Observable<any>{
+    const params = new HttpParams()
+    .set("item_per_page", itemPerPage.toString())
+    .set("page_number", pageNumber.toString())
+    .set('sort_order', sort)
+    .set('sort_by', sortBy)
+    .set('search', search)
+    .set('search_by', searchBy);
+    return this.httpClient.get<any>(`${this.baseUrl}/users/get/by-filters-leave-setting`, {params});
+  }
+
   getAllUsers(sort: string, sortBy: string, search: string, searchBy: string) : Observable<any>{
     const params = new HttpParams()
     .set('sortOrder', sort)
