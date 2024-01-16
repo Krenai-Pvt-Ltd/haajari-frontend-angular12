ddddc7efe96721149c7370dcfec091d4af3100f8
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2024-01-16 17:19:54 +0530

    ui fix

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -48,20 +48,12 @@ export class DataService {
     return this.orgIdEmitter;
   }
   
-<<<<<<< HEAD
   // private baseUrl = "http://localhost:8080/api/v2"
   // private baseUrl = "http://localhost:8080/api/v2";
 
-  // private baseUrl = "https://backend.hajiri.work/api/ v2";
-=======
+  private baseUrl = "https://backend.hajiri.work/api/v2";
 
-  private baseUrl = "http://localhost:8080/api/v2";
-
-
-  // private baseUrl = "https://backend.hajiri.work/api/v2";
->>>>>>> 497602e495ab0f05b66d44d65ddd33246433169c
-
-  private baseUrl = "https://production.hajiri.work/api/v2";
+  // private baseUrl = "https://production.hajiri.work/api/v2";
 
   openSidebar: boolean = true;
   registerOrganizationUsingCodeParam(codeParam: string): Observable<any>{
