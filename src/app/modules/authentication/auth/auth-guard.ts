b5ece1ef1ca316dd5cc9902513bbfc927d15d5aa
Shivendra Shrivastwa <shivendra@krenai.com>
2024-01-31 13:45:15 +0530

    bug fixed

--- src/app/modules/authentication/auth/auth-guard.ts
+++ src/app/modules/authentication/auth/auth-guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { Key } from "src/app/constant/key";
 import { RoleBasedAccessControlService } from "src/app/services/role-based-access-control.service";
 
 @Injectable({
@@ -12,6 +13,14 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const token = localStorage.getItem('token');
     if (!this.isValidTokenFormat(token)) {
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+
+    
+    if(this.rbacService.getRole()! == Key.USER){
+      debugger
+      const r = route;
       this.router.navigate(['/auth/login']);
       return false;
     }
