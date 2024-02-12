b30835e9dd3cd686b56adfa5be8ee16022594c0a
Richa <richa@krenai.com>
2024-02-12 16:42:08 +0530

    commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -50,11 +50,11 @@ export class DataService {
     return this.orgIdEmitter;
   }
   
-  // private baseUrl = "http://localhost:8080/api/v2";
+  private baseUrl = "http://localhost:8080/api/v2";
 
   // private baseUrl = "https://backend.hajiri.work/api/v2";
 
-  private baseUrl = "https://production.hajiri.work/api/v2";
+  // private baseUrl = "https://production.hajiri.work/api/v2";
 
   openSidebar: boolean = true;
   registerOrganizationUsingCodeParam(codeParam: string): Observable<any>{
@@ -704,6 +704,14 @@ export class DataService {
     const params = new HttpParams()
     .set("userUuid", userUuid);
     const url = `${this.baseUrl}/user-documents-details/get/user-documents`;
+    return this.httpClient.get(url, {params});
+  }
+
+  getEmployeeDocumentAsList(userUuid: string): Observable<any> {
+    debugger
+    const params = new HttpParams()
+    .set("userUuid", userUuid);
+    const url = `${this.baseUrl}/user-documents-details/get/user-documents-as-List`;
     return this.httpClient.get(url, {params});
   }
 
