925b0c6616d048137b37e9701ac6aea77fc1c211
Shivendra Shrivastwa <shivendra@krenai.com>
2024-03-07 18:22:21 +0530

    bug fixed

---
+++ src/app/models/salary-component-response.spec.ts
@@ -0,0 +1,7 @@
+import { SalaryComponentResponse } from './salary-component-response';
+
+describe('SalaryComponentResponse', () => {
+  it('should create an instance', () => {
+    expect(new SalaryComponentResponse()).toBeTruthy();
+  });
+});
