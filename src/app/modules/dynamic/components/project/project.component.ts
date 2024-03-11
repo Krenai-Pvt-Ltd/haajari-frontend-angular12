5a96981adfca5e994fad9bee5bfd7793f8e22b34
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2024-03-11 10:23:09 +0530

    fix

--- src/app/modules/dynamic/components/project/project.component.ts
+++ src/app/modules/dynamic/components/project/project.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem,CdkDrag,CdkDropList } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,7 +13,47 @@ export class ProjectComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
+  // todo:string[] = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
+
+  // done:string[] = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
+
+  // inpro:string[] = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
+
+  // drop(event: any) {
+  //   console.log("event==========",event);
+  //   // if (event.previousContainer === event.container) {
+  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  //   // } else {
+  //   //   transferArrayItem(
+  //   //     event.previousContainer.data,
+  //   //     event.container.data,
+  //   //     event.previousIndex,
+  //   //     event.currentIndex,
+  //   //   );
+  //   // }
+  // }
+
+
+  backlog = ['6', '5', '7', '8'];
+  done=['9', '10', '11','12','13','14'];
+  inprogress = ['1', '2','3','4'];
+  testing = ['15', '16', '17'];
+  todo = ['18','19'];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log("event=-======",event);
+    if (event.previousContainer == event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 
 }
 
