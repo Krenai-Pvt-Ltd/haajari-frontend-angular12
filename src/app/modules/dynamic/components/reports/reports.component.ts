6ce97362c5dfc0a3cd1535aab9742634d9141086
Richa <richa@krenai.com>
2024-03-19 15:36:08 +0530

    commit

--- src/app/modules/dynamic/components/reports/reports.component.ts
+++ src/app/modules/dynamic/components/reports/reports.component.ts
@@ -1,7 +1,11 @@
 import { DatePipe } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as moment from 'moment';
+import { Key } from 'src/app/constant/key';
 import { AttendanceReportLogs } from 'src/app/models/AttendanceReportLogs';
 import { DataService } from 'src/app/services/data.service';
+import { HelperService } from 'src/app/services/helper.service';
+import { RoleBasedAccessControlService } from 'src/app/services/role-based-access-control.service';
 
 @Component({
   selector: 'app-reports',
@@ -11,15 +15,19 @@ import { DataService } from 'src/app/services/data.service';
 export class ReportsComponent implements OnInit {
 
 
+  userUuid:string='';
   isModalVisible: boolean = false;
   isLoading: boolean = false;
   startDate: Date | null = null;
   endDate: Date | null = null;
+  organizationOnboardingDate:Date=new Date('YYYY-MM-dd');
 
-  constructor(private dataService: DataService,  private datePipe: DatePipe) { }
+  constructor(private dataService: DataService, private helperService: HelperService,  private datePipe: DatePipe, private rbacService: RoleBasedAccessControlService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.getAttendanceReportLogs();
+    this.userUuid = await this.rbacService.getUUID();
+    this.getOrganizationOnboardingDateByUuid();
   }
 
 
@@ -29,18 +37,69 @@ export class ReportsComponent implements OnInit {
     // this.isModalVisible = true;
     this.dateRangeModalClose.nativeElement.click();
   }
+  
+
+  getOrganizationOnboardingDateByUuid() {
+    debugger
+    this.dataService.getOrganizationOnboardingDate(this.userUuid).subscribe(
+      (data) => {
+        this.organizationOnboardingDate = new Date(data);
+        console.log("getOrganizationOnboardingDateByUuid", this.getOrganizationOnboardingDateByUuid);
+      },
+      (error) => {
+      }
+    );
+  }
+
+  selectedMonth:any;
+
+  // organizationOnboardingDate: Date = new Date('2023-11-01'); 
+
+  disableBeforeOnboarding = (current: Date): boolean => {
+    const onboardingMonth = new Date(this.organizationOnboardingDate.getFullYear(), this.organizationOnboardingDate.getMonth(), 1);
+    const currentMonth = new Date(current.getFullYear(), current.getMonth(), 1);
+    const now = new Date();
+    const currentEndOfMonth = new Date(current.getFullYear(), current.getMonth() + 1, 0);
+    return currentMonth < onboardingMonth || currentEndOfMonth > new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  };
 
   handleOk(): void {
-    this.isModalVisible = false;
+    if (this.selectedMonth) {
+      const startOfMonth = new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth(), 1);
+      const endOfMonth = new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth() + 1, 0); // Sets day as the last day of the month
+  
+      this.startDate = startOfMonth;
+      this.endDate = endOfMonth;
+  
+      console.log('Start Date:', this.startDate);
+      console.log('End Date:', this.endDate);
+
+      this.isModalVisible = false;
     if (this.startDate && this.endDate) {
-      debugger
       this.isLoading = true;
+      this.helperService.showToast("Please Wait! We're loading your Attendance Records.", Key.TOAST_STATUS_SUCCESS);
       let formattedStartDate = this.formatDate(this.startDate);
       let formattedEndDate = this.formatDate(this.endDate);
       this.generateAttendanceReport(formattedStartDate, formattedEndDate);
+     
       this.closeModal();
     }
+    this.selectedMonth="";
+    }
   }
+  
+
+  // handleOk(): void {
+  //   this.isModalVisible = false;
+  //   if (this.startDate && this.endDate) {
+  //     debugger
+  //     this.isLoading = true;
+  //     let formattedStartDate = this.formatDate(this.startDate);
+  //     let formattedEndDate = this.formatDate(this.endDate);
+  //     this.generateAttendanceReport(formattedStartDate, formattedEndDate);
+  //     this.closeModal();
+  //   }
+  // }
 
   handleCancel(): void {
     this.isModalVisible = false;
@@ -52,6 +111,8 @@ export class ReportsComponent implements OnInit {
         console.log('Report Generation Successful', response);
         this.isLoading = false;
         this.getAttendanceReportLogs();
+        this.helperService.showToast("Attendance Records Fetched Successfully!", Key.TOAST_STATUS_SUCCESS);
+
       },
       error: (error) => {
         console.error('Error generating report', error);
@@ -89,14 +150,21 @@ export class ReportsComponent implements OnInit {
   // }
 
   attendanceReportLogs: AttendanceReportLogs[] = [];
-
+  isAttendanceLogsPlaceholder:boolean=false;
   getAttendanceReportLogs(): void {
     this.dataService.getAttendanceReportLogs().subscribe({
       next: (response) => {
+        console.log('userUuid', this.userUuid);
         console.log('Logs Generation Successful', response);
         this.attendanceReportLogs = response.listOfObject;
+        if(this.attendanceReportLogs.length===0){
+        this.isAttendanceLogsPlaceholder=true;
+        }else{
+          this.isAttendanceLogsPlaceholder=false;
+        }
       },
       error: (error) => {
+        this.isAttendanceLogsPlaceholder=false;
         console.error('Error generating report', error);
       }
     });
