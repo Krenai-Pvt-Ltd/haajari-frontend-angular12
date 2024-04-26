b8c3ef61e9f901f2a1b0bbe635fe1e8478ddb975
Shivendra Shrivastwa <shivendra@krenai.com>
2024-04-26 11:00:38 +0530

    onboarding step in auth guard handled

--- src/app/modules/authentication/auth/auth-guard.ts
+++ src/app/modules/authentication/auth/auth-guard.ts
@@ -1,42 +1,68 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
-import { Key } from "src/app/constant/key";
-import { RoleBasedAccessControlService } from "src/app/services/role-based-access-control.service";
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Key } from 'src/app/constant/key';
+import { RoleBasedAccessControlService } from 'src/app/services/role-based-access-control.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private rbacService: RoleBasedAccessControlService,
+  ) {}
 
-  constructor(private router: Router, private rbacService: RoleBasedAccessControlService) {}
-
-  UUID:any;
-  ROLE:any;
+  UUID: any;
+  ROLE: any;
+  ONBOARDING_STEP: any;
   async ngOnInit(): Promise<void> {
     this.UUID = await this.rbacService.getUUID();
     this.ROLE = await this.rbacService.getRole();
-
+    this.ONBOARDING_STEP = await this.rbacService.getOnboardingStep();
   }
 
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Promise<boolean> {
     const token = localStorage.getItem('token');
-    if (! await (this.isValidTokenFormat(token))) {
+    if (!(await this.isValidTokenFormat(token))) {
       this.router.navigate(['/auth/login']);
       return false;
     }
 
+    if (this.isOrganizationOnboarded(this.ONBOARDING_STEP)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/organization-onboarding/personal-information']);
+    }
+
     await this.rbacService.isUserInfoInitialized();
 
-    if(route !== null && route.routeConfig !== null){
-
-      if(await this.rbacService.getRole() == Key.USER && route.routeConfig.path == 'dashboard') {
-        this.router.navigate(['/employee-profile'], {queryParams : {"userId" : await this.rbacService.getUUID(), dashboardActive: 'true' }});
+    if (route !== null && route.routeConfig !== null) {
+      if (
+        (await this.rbacService.getRole()) == Key.USER &&
+        route.routeConfig.path == 'dashboard'
+      ) {
+        this.router.navigate(['/employee-profile'], {
+          queryParams: {
+            userId: await this.rbacService.getUUID(),
+            dashboardActive: 'true',
+          },
+        });
         return false;
       }
 
-      const requiredSubmodule = '/'+route.routeConfig.path;
-      if (requiredSubmodule && !(await this.rbacService.hasAccessToSubmodule(requiredSubmodule))) {
+      const requiredSubmodule = '/' + route.routeConfig.path;
+      if (
+        requiredSubmodule &&
+        !(await this.rbacService.hasAccessToSubmodule(requiredSubmodule))
+      ) {
         this.router.navigate(['/unauthorized']);
         return false;
       }
@@ -45,7 +71,6 @@ export class AuthGuard implements CanActivate {
     return true;
   }
 
-  
   async isValidTokenFormat(token: string | null): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
       try {
@@ -53,7 +78,7 @@ export class AuthGuard implements CanActivate {
           resolve(false);
           return;
         }
-  
+
         const parts = token.split('.');
         resolve(parts.length === 3);
       } catch (error) {
@@ -61,5 +86,12 @@ export class AuthGuard implements CanActivate {
       }
     });
   }
-  
+
+  isOrganizationOnboarded(onboardingStep: number): boolean {
+    if (onboardingStep == Key.REGISTRATION_COMPLETED_STEP) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
