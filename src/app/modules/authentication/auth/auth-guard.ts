72015424823b14b6ca6586154342acbad0467aa5
Shivendra Shrivastwa <shivendra@krenai.com>
2024-04-29 12:53:18 +0530

    bug fixed

--- src/app/modules/authentication/auth/auth-guard.ts
+++ src/app/modules/authentication/auth/auth-guard.ts
@@ -36,15 +36,13 @@ export class AuthGuard implements CanActivate {
       return false;
     }
 
-    if (this.isOrganizationOnboarded(this.ONBOARDING_STEP)) {
-      debugger
-      console.log(this.ONBOARDING_STEP);
-      this.router.navigate(['/dashboard']);
-    } else {
-      debugger
-      console.log(6);
-      this.router.navigate(['/organization-onboarding/personal-information']);
-    }
+    // if (this.isOrganizationOnboarded(this.ONBOARDING_STEP)) {
+    //   console.log(this.ONBOARDING_STEP);
+    //   this.router.navigate(['/dashboard']);
+    // } else {
+    //   console.log(this.ONBOARDING_STEP);
+    //   this.router.navigate(['/organization-onboarding/personal-information']);
+    // }
 
     await this.rbacService.isUserInfoInitialized();
 
