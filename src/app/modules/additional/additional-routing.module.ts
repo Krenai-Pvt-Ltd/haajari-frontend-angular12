fad2f2b3c5f0d8a75cbbcc23086873c288c1febc
Shivendra Shrivastwa <shivendra@krenai.com>
2024-05-31 10:35:31 +0530

    slack-installation-successfull route added

--- src/app/modules/additional/additional-routing.module.ts
+++ src/app/modules/additional/additional-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdditionalComponent } from './additional.component';
 import { LeaveRequestFormComponent } from './components/leave-request-form/leave-request-form.component';
+import { SlackInstallationSuccessfullComponent } from './components/slack-installation-successfull/slack-installation-successfull.component';
 
 const routes: Routes = [{ path: '', redirectTo: '/additional/leave-request', pathMatch:'full'},
   { path: '', component: AdditionalComponent,
 
   children:[
-  {path: 'leave-request', component: LeaveRequestFormComponent}
+  {path: 'leave-request', component: LeaveRequestFormComponent},
+  {path: 'slack-installation-successfull', component: SlackInstallationSuccessfullComponent}
   ] }
 ];
 
