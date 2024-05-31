5cddf56de6157a7e6b9896630203f9a3171de081
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2024-05-31 15:22:50 +0530

    bug fixed

--- src/app/modules/additional/additional-routing.module.ts
+++ src/app/modules/additional/additional-routing.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdditionalComponent } from './additional.component';
+import { InternalServerErrorPageComponent } from './components/internal-server-error-page/internal-server-error-page.component';
 import { LeaveRequestFormComponent } from './components/leave-request-form/leave-request-form.component';
 import { SlackInstallationSuccessfullComponent } from './components/slack-installation-successfull/slack-installation-successfull.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/additional/leave-request', pathMatch:'full'},
-  { path: '', component: AdditionalComponent,
+const routes: Routes = [
+  { path: '', redirectTo: '/additional/leave-request', pathMatch: 'full' },
+  {
+    path: '',
+    component: AdditionalComponent,
 
-  children:[
-  {path: 'leave-request', component: LeaveRequestFormComponent},
-  {path: 'slack-installation-successfull', component: SlackInstallationSuccessfullComponent}
-  ] }
+    children: [
+      { path: 'leave-request', component: LeaveRequestFormComponent },
+      {
+        path: 'slack-installation-successfull',
+        component: SlackInstallationSuccessfullComponent,
+      },
+      {
+        path: 'internal-server-error',
+        component: InternalServerErrorPageComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdditionalRoutingModule { }
+export class AdditionalRoutingModule {}
