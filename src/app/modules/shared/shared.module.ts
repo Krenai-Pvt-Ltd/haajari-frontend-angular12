8a5fde8174178ff2eab6ddcacfbb4967bc8290eb
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2024-05-31 10:58:52 +0530

    bug fixed

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { HelperComponent } from './helper/helper.component';
 import { NotifactionTostComponent } from 'src/app/modules/sharable/notifaction-toast/notifaction-toast.component';
 import { LogoutConfirmationModalComponent } from './logout-confirmation-modal/logout-confirmation-modal.component';
-import { SlackOnboardingSuccessPageComponent } from './slack-onboarding-success-page/slack-onboarding-success-page.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedComponent } from './shared.component';
@@ -17,7 +16,6 @@ import { SharedComponent } from './shared.component';
     SharedComponent,
     HelperComponent,
     LogoutConfirmationModalComponent,
-    SlackOnboardingSuccessPageComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule, SharedRoutingModule],
   exports: [],
