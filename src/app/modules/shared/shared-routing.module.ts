42f3d693596d9a208a13201d806bd006e45c35f5
Shivendra Shrivastwa <shivendra@krenai.com>
2024-05-31 10:48:09 +0530

    bug fixed

--- src/app/modules/shared/shared-routing.module.ts
+++ src/app/modules/shared/shared-routing.module.ts
@@ -1,25 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedComponent } from './shared.component';
-import { SlackOnboardingSuccessPageComponent } from './slack-onboarding-success-page/slack-onboarding-success-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/shared/slack-installation-successfull',
-    pathMatch: 'full',
-  },
-  {
-    path: '',
-    component: SharedComponent,
-
-    children: [
-      {
-        path: 'slack-installation-successfull',
-        component: SlackOnboardingSuccessPageComponent,
-      },
-    ],
-  },
 ];
 
 @NgModule({
