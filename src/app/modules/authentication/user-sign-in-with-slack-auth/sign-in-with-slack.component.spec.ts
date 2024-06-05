3767b1fcb371e04af124ad448c711b21f996a331
Richa <richa@krenai.com>
2024-06-05 16:11:55 +0530

    commit

---
+++ src/app/modules/authentication/user-sign-in-with-slack-auth/sign-in-with-slack.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignInWithSlackComponent } from './sign-in-with-slack.component';
+
+describe('SignInWithSlackComponent', () => {
+  let component: SignInWithSlackComponent;
+  let fixture: ComponentFixture<SignInWithSlackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SignInWithSlackComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignInWithSlackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
