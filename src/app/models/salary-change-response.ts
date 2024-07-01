b850a3e8dde9f72051d3f1ed87fb51f08f8d13fc
Shivendra Shrivastwa <shivendra@krenai.com>
2024-07-01 19:10:29 +0530

    bug fixed

---
+++ src/app/models/salary-change-response.ts
@@ -0,0 +1,8 @@
+export class SalaryChangeResponse {
+    uuid : string = '';
+    name : string = '';
+    email : string = '';
+    previousCTC : string = '';
+    updatedCTC : string = '';
+    effectiveDate : string = '';
+}
