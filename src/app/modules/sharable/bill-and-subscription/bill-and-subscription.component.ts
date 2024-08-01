579f8b1856319c961e1172df3bc77cefd527f2ae
Lakshay Gautam <lakshay.gautam@krenai.com>
2024-08-01 18:42:38 +0530

    commit

---
+++ src/app/modules/sharable/bill-and-subscription/bill-and-subscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-and-subscription',
+  templateUrl: './bill-and-subscription.component.html',
+  styleUrls: ['./bill-and-subscription.component.css']
+})
+export class BillAndSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
