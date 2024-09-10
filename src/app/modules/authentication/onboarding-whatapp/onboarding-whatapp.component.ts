3db9bc17079ff79ce6f9498268abc0862f891778
krenaiYogesh <129038607+krenaiYogesh@users.noreply.github.com>
2024-09-11 00:32:40 +0530

    new-ui-design

---
+++ src/app/modules/authentication/onboarding-whatapp/onboarding-whatapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding-whatapp',
+  templateUrl: './onboarding-whatapp.component.html',
+  styleUrls: ['./onboarding-whatapp.component.css']
+})
+export class OnboardingWhatappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
