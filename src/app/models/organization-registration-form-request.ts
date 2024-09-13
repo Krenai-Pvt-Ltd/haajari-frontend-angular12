683fa4f6d7dbe57c1eaeea3514026f3d2733b448
Richa Dixit <richa@krenai.com>
2024-09-13 15:31:34 +0530

    commit

---
+++ src/app/models/organization-registration-form-request.ts
@@ -0,0 +1,9 @@
+export class OrganizationRegistrationFormRequest {
+    organizationName!: string;
+    employeeCount!: number;
+    industryType!: string;
+    organizationEmail!: string;
+    firstName!: string;
+    lastName!: string;
+    phoneNumber!: string;
+}
--- src/app/modules/authentication/organization-registration-form/organization-registration-form.component.ts
+++ src/app/modules/authentication/organization-registration-form/organization-registration-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrganizationRegistrationFormRequest } from 'src/app/models/organization-registration-form-request';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-organization-registration-form',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService : DataService) { }
 
   ngOnInit(): void {
   }
+
+  formData: OrganizationRegistrationFormRequest = {
+    organizationName: '',
+    employeeCount: 0,
+    industryType: '',
+    organizationEmail: '',
+    firstName: '',
+    lastName: '',
+    phoneNumber: ''
+  };
+
+
+  onSubmit(): void {
+    if (this.formData) {
+      this.dataService.registerOrganizationRegistrationFormInfo(this.formData).subscribe(
+        response => {
+          console.log('Registration successful', response);
+        },
+        error => {
+          console.error('Registration failed', error);
+        }
+      );
+    }
+  }
+
 
 }
