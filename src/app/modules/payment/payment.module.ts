c36131286a197f6a638dd9c17e3f99109fedbebd
AnchalSharma77 <anchal6899@gmail.com>
2024-12-04 14:03:09 +0530

    chart height and anme fixed

--- src/app/modules/payment/payment.module.ts
+++ src/app/modules/payment/payment.module.ts
@@ -9,7 +9,6 @@ import { TdsComponent } from './tds/tds.component';
 import { BonusAndDeductionComponent } from './bonus-and-deduction/bonus-and-deduction.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { DynamicModule } from '../dynamic/dynamic.module';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { DateFormatterPipe } from 'src/app/pipe/date-formatter.pipe';
@@ -19,7 +18,6 @@ import { NgxShimmerLoadingModule } from 'ngx-shimmer-loading';
 import { LeaveSummaryComponent } from './leave-summary/leave-summary.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { DurationFormatPipe } from '../dynamic/duration-format.pipe';
 
 @NgModule({
   declarations: [
