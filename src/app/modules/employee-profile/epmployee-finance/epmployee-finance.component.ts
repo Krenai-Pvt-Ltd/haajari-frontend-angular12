add2106ed65008ea37ef954d30cbbce66f119e1f
yogesh <yogesh@krenai.com>
2024-12-19 19:13:34 +0530

    fix

---
+++ src/app/modules/employee-profile/epmployee-finance/epmployee-finance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-epmployee-finance',
+  templateUrl: './epmployee-finance.component.html',
+  styleUrls: ['./epmployee-finance.component.css']
+})
+export class EpmployeeFinanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
