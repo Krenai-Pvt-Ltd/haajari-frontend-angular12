d838ce2e39c4702b53e7e2b96b203b9f7cfcfc7c
yogesh <yogesh@krenai.com>
2024-12-26 17:34:30 +0530

    fix

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -1,8 +1,8 @@
-export class constant{
+export class constant {
 
     static EMPTY_STRINGS = [null, undefined, '', 'N/A', 'n/a', ' ', 'null', 'undefined'];
 
-    public static ALLOWED_BULK_UPLOAD_FORMATS=['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.ms-excel'];
+    public static ALLOWED_BULK_UPLOAD_FORMATS = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'];
 
     public static REMOVE_SHIFT_STRING = 'Keep In - ';
     public static USER = 'USER';
@@ -23,7 +23,7 @@ export class constant{
     public static SETTING_SUBSCRIPTION_ROUTE = "/setting/subscription";
     public static LOGIN_ROUTE = "/auth/login";
 
-    public static ONBOARDING_ROUTES =[constant.ORG_ONBOARDING_PERSONAL_INFORMATION_ROUTE, constant.ORG_ONBOARDING_EMPLOYEE_CREATION_ROUTE, constant.ORG_ONBOARDING_SHIFT_TIME_ROUTE,
+    public static ONBOARDING_ROUTES = [constant.ORG_ONBOARDING_PERSONAL_INFORMATION_ROUTE, constant.ORG_ONBOARDING_EMPLOYEE_CREATION_ROUTE, constant.ORG_ONBOARDING_SHIFT_TIME_ROUTE,
     constant.ORG_ONBOARDING_ATTENDANCE_MODE_ROUTE
     ]
 }
