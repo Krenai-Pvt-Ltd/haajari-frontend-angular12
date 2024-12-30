3b3e8134c38eb255a1c137f66e31a05336510cbe
Abhijeet262 <abhijeet.verma@krenai.com>
2024-12-30 19:06:52 +0530

    empployee finance

---
+++ src/app/models/UserSalaryRevisionRes.ts
@@ -0,0 +1,10 @@
+import { EmployeePayslipBreakupResponse } from "./employee-payslip-breakup-response";
+
+export class UserSalaryRevisionRes{
+
+ effectiveDate:string='';
+ annualCtc:number=0;
+ monthlyCtc:number=0;
+ salaryBreakup : EmployeePayslipBreakupResponse [] = [];
+
+}
