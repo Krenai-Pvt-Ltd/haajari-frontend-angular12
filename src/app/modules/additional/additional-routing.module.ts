d231de0171af83fdb279cc727966ef25da3bfce7
AnchalSharma77 <anchal6899@gmail.com>
2025-01-28 19:12:10 +0530

    fX

--- src/app/modules/additional/additional-routing.module.ts
+++ src/app/modules/additional/additional-routing.module.ts
@@ -4,7 +4,6 @@ import { AdditionalComponent } from './additional.component';
 import { InternalServerErrorPageComponent } from './components/internal-server-error-page/internal-server-error-page.component';
 import { LeaveRequestFormComponent } from './components/leave-request-form/leave-request-form.component';
 import { SlackInstallationSuccessfullComponent } from './components/slack-installation-successfull/slack-installation-successfull.component';
-import { EmployeeLocationValidatorComponent } from './components/employee-location-validator/employee-location-validator.component';
 import { EmployeeAttendancePhotoComponent } from './components/employee-attendance-photo/employee-attendance-photo.component';
 import { AttendanceUrlComponent } from './components/attendance-url/attendance-url.component';
 
@@ -18,8 +17,10 @@ const routes: Routes = [
       { path: 'leave-request', component: LeaveRequestFormComponent },
       { path: 'slack-installation-successfull', component: SlackInstallationSuccessfullComponent },
       { path: 'internal-server-error', component: InternalServerErrorPageComponent },
-      { path: 'location-validator', component: EmployeeLocationValidatorComponent },
+      { path: 'location-validator', component: AttendanceUrlComponent },
+      // EmployeeLocationValidatorComponent - deprecated
       { path: 'attendance-photo', component: EmployeeAttendancePhotoComponent },
+      //EmployeeAttendancePhotoComponent - deprecated
       { path: 'attendance-url', component: AttendanceUrlComponent },
     ],
   },
