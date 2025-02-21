5612187e1df5a5a3834cd9fc5146d225154cc12c
yogesh <yogesh@krenai.com>
2025-02-21 18:59:20 +0530

    fx

--- src/app/modules/payroll/component/payroll-setup/payroll-setup.component.ts
+++ src/app/modules/payroll/component/payroll-setup/payroll-setup.component.ts
@@ -17,55 +17,55 @@ export class PayrollSetupComponent implements OnInit {
       "id": 1,
       "name": "Organization Profile Setup",
       "description": "Set up your organization details and work locations for accurate payroll processing.",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-a",
-      "active": 0
+      "active": false
     },
     {
       "id": 2,
       "name": "Pay Schedule Configuration",
       "description": "Define payroll cycles, payment dates, and salary disbursement frequency.",
-      "isComplete": 1,
+      "isComplete": true,
       "route": "/task-b",
-      "active": 0
+      "active": false
     },
     {
       "id": 3,
       "name": "Statutory Compliance Setup",
       "description": "Enable and configure EPF, ESI, PT, LWF, and other legal deductions.",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-c",
-      "active": 0
+      "active": false
     },
     {
       "id": 4,
       "name": "Salary Components Management",
       "description": "Customize salary structures, allowances, and deductions as per company policy. ",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-d",
-      "active": 0
+      "active": false
     },
     {
       "id": 5,
       "name": "Taxes Configuration",
       "description": "Provide necessary tax details to ensure compliance with statutory regulations.",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-d",
-      "active": 0
+      "active": false
     },{
       "id": 6,
       "name": "Set Up Salary Template",
       "description": "Standardize payroll processing with predefined templates.",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-d",
-      "active": 0
+      "active": false
     },{
       "id": 7,
       "name": "Previous Payroll Import",
       "description": "Upload or configure prior payroll data for seamless payroll continuity.",
-      "isComplete": 0,
+      "isComplete": false,
       "route": "/task-d",
-      "active": 0
+      "active": false
     }
   ]
   ;
@@ -73,11 +73,11 @@ export class PayrollSetupComponent implements OnInit {
 
   activeStep(id:number){
     this.toDoStepList.forEach(step=>{
-      step.active = 0;
+      step.active = false;
     });
     var index = this.toDoStepList.findIndex(x=> x.id == id);
     if(index> -1){
-      this.toDoStepList[index].active = 1;
+      this.toDoStepList[index].active = true;
     }
   }
 
