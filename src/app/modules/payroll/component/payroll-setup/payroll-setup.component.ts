c6a22430603de91945f8d985ce0fe93fffa8dfd9
yogesh <yogesh@krenai.com>
2025-02-21 14:09:02 +0530

    fx

--- src/app/modules/payroll/component/payroll-setup/payroll-setup.component.ts
+++ src/app/modules/payroll/component/payroll-setup/payroll-setup.component.ts
@@ -12,4 +12,73 @@ export class PayrollSetupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toDoStepList = [
+    {
+      "id": 1,
+      "name": "Organization Profile Setup",
+      "description": "Set up your organization details and work locations for accurate payroll processing.",
+      "isComplete": 0,
+      "route": "/task-a",
+      "active": 0
+    },
+    {
+      "id": 2,
+      "name": "Pay Schedule Configuration",
+      "description": "Define payroll cycles, payment dates, and salary disbursement frequency.",
+      "isComplete": 1,
+      "route": "/task-b",
+      "active": 0
+    },
+    {
+      "id": 3,
+      "name": "Statutory Compliance Setup",
+      "description": "Enable and configure EPF, ESI, PT, LWF, and other legal deductions.",
+      "isComplete": 0,
+      "route": "/task-c",
+      "active": 0
+    },
+    {
+      "id": 4,
+      "name": "Salary Components Management",
+      "description": "Customize salary structures, allowances, and deductions as per company policy. ",
+      "isComplete": 0,
+      "route": "/task-d",
+      "active": 0
+    },
+    {
+      "id": 5,
+      "name": "Taxes Configuration",
+      "description": "Provide necessary tax details to ensure compliance with statutory regulations.",
+      "isComplete": 0,
+      "route": "/task-d",
+      "active": 0
+    },{
+      "id": 6,
+      "name": "Set Up Salary Template",
+      "description": "Standardize payroll processing with predefined templates.",
+      "isComplete": 0,
+      "route": "/task-d",
+      "active": 0
+    },{
+      "id": 7,
+      "name": "Previous Payroll Import",
+      "description": "Upload or configure prior payroll data for seamless payroll continuity.",
+      "isComplete": 0,
+      "route": "/task-d",
+      "active": 0
+    }
+  ]
+  ;
+  
+
+  activeStep(id:number){
+    this.toDoStepList.forEach(step=>{
+      step.active = 0;
+    });
+    var index = this.toDoStepList.findIndex(x=> x.id == id);
+    if(index> -1){
+      this.toDoStepList[index].active = 1;
+    }
+  }
+
 }
