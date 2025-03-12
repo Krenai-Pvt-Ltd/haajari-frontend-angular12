4f36cdfa0bec7c1f01f6e531eee0df25545a0dd8
yogesh <yogesh@krenai.com>
2025-03-12 19:10:47 +0530

    fx

--- src/app/modules/payroll/component/salary-template/salary-template.component.ts
+++ src/app/modules/payroll/component/salary-template/salary-template.component.ts
@@ -12,4 +12,53 @@ export class SalaryTemplateComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+    templates = [
+      {
+        name: 'Internship',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: true
+      },
+      {
+        name: 'Fresher',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. ',
+        status: false
+      },
+      {
+        name: 'SDE 1',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: true
+      },
+      {
+        name: 'SDE 2',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: true
+      },
+      {
+        name: 'SDE 3',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: false
+      },
+      {
+        name: 'Internship 2',
+        date: '12th June, 2024',
+        description: 'Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: true
+      },
+      {
+        name: 'Fresher 2',
+        date: '12th June, 2024',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        status: true
+      }
+    ];
+  
+    toggleStatus(index: number) {
+      this.templates[index].status = !this.templates[index].status;
+    }
 }
