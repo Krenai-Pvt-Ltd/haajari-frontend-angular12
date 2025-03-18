5c39b5616c190ea423ff8868825c5e011e19c4d6
yogesh <yogesh@krenai.com>
2025-03-18 17:48:02 +0530

    fx

--- src/app/modules/payroll/component/configuration/configuration.component.ts
+++ src/app/modules/payroll/component/configuration/configuration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PayrollTodoStep } from 'src/app/payroll-models/PayrollTodoStep';
-import { PayrollConfigurationService } from 'src/app/services/payroll-configuration.service';
+import { ProfessionalTax } from 'src/app/payroll-models/ProfeessionalTax';
+
+
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
@@ -10,52 +10,121 @@ import { PayrollConfigurationService } from 'src/app/services/payroll-configurat
 export class ConfigurationComponent implements OnInit {
 
 
-  constructor(private _payrollConfigurationService :PayrollConfigurationService,
-    private activateRoute: ActivatedRoute,
-    private router: Router
-  ) {
 
-    if (this.activateRoute.snapshot.queryParamMap.has('tab')) {
-      this.currentTab = String(this.activateRoute.snapshot.queryParamMap.get('tab'));
-    }
-  }
+  isDivVisible: boolean = false;
 
   ngOnInit(): void {
-    window.scroll(0,0);
-    this.getTodoList();
+    
   }
 
-  currentTab: string= 'profile';
-  route(tabName: string) {
-    this.router.navigate(['/payroll/configuration'], {
-      queryParams: { tab: tabName },
-    });
-    this.currentTab=tabName;
+  toggleDiv() {
+    this.isDivVisible = !this.isDivVisible;
   }
-
-  toDoStepList:PayrollTodoStep[]=new Array();
-   getTodoList() {
-
-      this._payrollConfigurationService.getTodoList().subscribe(
-        (response) => {
-          if(response.status){
-            this.toDoStepList = response.object;
-            this.checkAllCompleted();
-
-          }
-        },
-        (error) => {
+  closeStatutoryDiv() {
+    this.isDivVisible = false;
+    this.tab= '';
+  }
+  stateList=[
+    { "id": 1, "name": "Andhra Pradesh" },
+    { "id": 2, "name": "Arunachal Pradesh" },
+    { "id": 3, "name": "Assam" },
+    { "id": 4, "name": "Bihar" },
+    { "id": 5, "name": "Chhattisgarh" },
+    { "id": 6, "name": "Goa" },
+    { "id": 7, "name": "Gujarat" },
+    { "id": 8, "name": "Haryana" },
+    { "id": 9, "name": "Himachal Pradesh" },
+    { "id": 10, "name": "Jharkhand" },
+    { "id": 11, "name": "Karnataka" },
+    { "id": 12, "name": "Kerala" },
+    { "id": 13, "name": "Madhya Pradesh" },
+    { "id": 14, "name": "Maharashtra" },
+    { "id": 15, "name": "Manipur" },
+    { "id": 16, "name": "Meghalaya" },
+    { "id": 17, "name": "Mizoram" },
+    { "id": 18, "name": "Nagaland" },
+    { "id": 19, "name": "Odisha" },
+    { "id": 20, "name": "Punjab" },
+    { "id": 21, "name": "Rajasthan" },
+    { "id": 22, "name": "Sikkim" },
+    { "id": 23, "name": "Tamil Nadu" },
+    { "id": 24, "name": "Telangana" },
+    { "id": 25, "name": "Tripura" },
+    { "id": 26, "name": "Uttar Pradesh" },
+    { "id": 27, "name": "Uttarakhand" },
+    { "id": 28, "name": "West Bengal" }
+  ]
   
+  selectedLocation: string = 'india';
+
+    selectedCurrency: string = 'INR'; // INR selected by default
+  
+    stateCurrency = [
+      { "code": "USD", "name": "USD", "symbol": "$" },
+      { "code": "EUR", "name": "Euro", "symbol": "€" },
+      { "code": "INR", "name": "INR", "symbol": "₹" }, // Default selection
+      { "code": "GBP", "name": "British Pound Sterling", "symbol": "£" },
+      { "code": "AUD", "name": "Australian Dollar", "symbol": "A$" },
+      { "code": "CAD", "name": "Canadian Dollar", "symbol": "C$" },
+      { "code": "SGD", "name": "Singapore Dollar", "symbol": "S$" },
+      { "code": "JPY", "name": "Japanese Yen", "symbol": "¥" },
+      { "code": "CNY", "name": "Chinese Yuan", "symbol": "¥" },
+      { "code": "CHF", "name": "Swiss Franc", "symbol": "CHF" },
+      { "code": "HKD", "name": "Hong Kong Dollar", "symbol": "HK$" },
+      { "code": "NZD", "name": "New Zealand Dollar", "symbol": "NZ$" },
+      { "code": "SEK", "name": "Swedish Krona", "symbol": "kr" },
+      { "code": "KRW", "name": "South Korean Won", "symbol": "₩" },
+      { "code": "BRL", "name": "Brazilian Real", "symbol": "R$" },
+      { "code": "ZAR", "name": "South African Rand", "symbol": "R" },
+      { "code": "RUB", "name": "Russian Ruble", "symbol": "₽" },
+      { "code": "MXN", "name": "Mexican Peso", "symbol": "$" },
+      { "code": "IDR", "name": "Indonesian Rupiah", "symbol": "Rp" },
+      { "code": "TRY", "name": "Turkish Lira", "symbol": "₺" },
+      { "code": "SAR", "name": "Saudi Riyal", "symbol": "﷼" },
+      { "code": "AED", "name": "United Arab Emirates Dirham", "symbol": "د.إ" }
+    ];
+
+
+    isLopChecked:boolean = true;
+    toggleLOPVisibility(): void {
+      console.log("toggled",this.isLopChecked)
+    }
+
+    calculateValue(type: string, value: number): string {
+      if (this.isLopChecked) {
+        if (type === 'basic') {
+          return ` ${(value * 0.85).toFixed(2)}`; 
         }
-      );
-    }
-    checkAllCompleted(): boolean {
-      return this.toDoStepList.every(step => step.completed);
+        if (type === 'transport') {
+          return ` ${(value * 0.90).toFixed(2)}`; 
+        }
+      }
+      return ` ${value.toFixed(2)}`;
     }
 
+    selectedPfWage = "12% of Actual PF Wage"; // Default selected value
 
+employer = [
+  { label: "12% of Actual PF Wage", value: "12% of Actual PF Wage" },
+  { label: "10% of Actual PF Wage", value: "10% of Actual PF Wage" }
+];
+employee = [
+  { label: "12% of Actual PF Wage", value: "12% of Actual PF Wage" },
+  { label: "10% of Actual PF Wage", value: "10% of Actual PF Wage" }
+];
+
+tab: string = '';
+switchTab(tab: string) {
+  this.tab = tab
 }
 
 
-  
+selectedTaxSlab!: ProfessionalTax; 
 
+
+
+  }
+
+
+
+  
