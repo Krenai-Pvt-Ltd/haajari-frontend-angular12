b8c0e4800c83a21ed32c41989298a23aabd870c5
Abhijeet262 <abhijeet.verma@krenai.com>
2025-03-28 13:28:02 +0530

    fx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,13 +23,8 @@ export const RAZOR_PAY_KEY: string = 'rzp_test_Wd1RYd0fng3673';
 // export const BASE_URL = 'http://localhost:8080/api/v2';
 
 // /* ------------------  Staging urls ----------------*/
-<<<<<<< HEAD
 // export const BASE_URL = 'https://staging.hajiri.work/api/v2';
  
-=======
-//  export const BASE_URL = 'https://staging.hajiri.work/api/v2';
-
->>>>>>> 8eaf0694278857dcd4391c7c67ef0e45ea774226
 /* ------------------  Production urls ----------------*/
 export const BASE_URL = 'https://production.hajiri.work/api/v2';
 
