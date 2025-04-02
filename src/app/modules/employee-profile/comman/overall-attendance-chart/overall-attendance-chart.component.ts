639d60d9aa14f41e2a80bd945e97a290263ba3b2
yogesh <yogesh@krenai.com>
2025-04-02 19:18:23 +0530

    fx

---
+++ src/app/modules/employee-profile/comman/overall-attendance-chart/overall-attendance-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-attendance-chart',
+  templateUrl: './overall-attendance-chart.component.html',
+  styleUrls: ['./overall-attendance-chart.component.css']
+})
+export class OverallAttendanceChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
